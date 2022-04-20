5ce90158f1a3d8281ec8543a6d209e90384a5a4e
Rubidubi <aprilianaayu14@gmail.com>
2022-04-20 11:51:02 +0700

    luas bola & kubus

---
+++ luas bola/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    float a, hasil, PHI=3.14;
+    printf(">>> LUAS PERMUKAAN BOLA <<<\n\n");
+    printf("Masukkan jari-jari = ");
+    scanf("%f", &a);
+    hasil=4*PHI*a*a;
+    printf("\nLuas permukaan lingkaran dengan panjang jari-jari %2.fcm adalah = %f2.cm\n\n\n", a, hasil);
+}
---
+++ luas kubus/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+    float a, hasil;
+    printf(">>> LUAS KUBUS <<<\n\n");
+    printf("Masukkan panjang sisi kubus = ");
+    scanf("%f", &a);
+    hasil=a*a;
+    printf("\nLuas kubus dengan panjang sisi %2.fcm adalah = %2.fcm\n\n\n", a, hasil);
+}
